d98e5e4f7672bbaf8a808c31ec7ea14dfc44fb38
Tommy <tommychoa@outlook.com>
2023-07-18 15:41:56 +0800

    finished proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,70 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+import Tours from "./components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [tourData, setTourData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const removeTour = (id) => {
+    const newTours = tourData.filter((tour) => tour.id !== id);
+    setTourData(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Failed to fetch tour data.");
+      }
+      const data = await response.json();
+      setTourData(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  console.log(tourData);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  //TODO
+  if (tourData.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            className="btn"
+            style={{ marginTop: "2rem" }}
+            onClick={() => fetchTours()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tourData={tourData} removeTour={removeTour} />
+    </main>
+  );
 };
+
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading">{/* <h4>Data is being gathered!</h4> */}</div>
+  );
+};
+
+export default Loading;
